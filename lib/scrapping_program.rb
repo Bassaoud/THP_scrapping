b6f621b447669c4021eacb2e795c62d048599637
Bassaoud <mohamed.bassaoud@yahoo.fr>
2019-07-18 17:17:04 +0200

    mise à jour

--- lib/scrapping_program.rb
+++ lib/scrapping_program.rb
@@ -3,27 +3,36 @@ require 'open-uri'
 
 
 
-def crypto_symbol(page)
+def crypto_symbol
 	array_symbol = []
+	page = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/"))
 	page.xpath('//*[@class = "text-left col-symbol"]').each do |momo|
 	array_symbol << momo.text
 	end
 	return array_symbol
 end
 
-def crypto_price(page)
+def crypto_price
 	array_price = []
+	page = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/"))
 	page.xpath('//*[@class="price"]').each do |price|
 	array_price << price.text
 	end
 	return array_price
 end 
 
+def concatenate(crypto_symbol, crypto_price)
+	full_array = crypto_symbol.zip(crypto_price)
+	array_of_hashs = []
+	full_array.each {|record| array_of_hashs << {record[0] => record[1]}}
+	return array_of_hashs
+end
+
 def perform
-	page = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/"))
-	array_test = crypto_symbol(page).zip(crypto_price(page))
-    hashing = Hash[array_test.map{ |key, value| [key, value] } ]
-	puts hashing
+	crypto_symbol
+	crypto_price
+	puts concatenate(crypto_symbol, crypto_price)
+
 end
 
 perform
